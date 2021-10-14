648c6a4ec26d92afda612ed161eb75cfaa908391
WildanPrasetyoK193040146 <193040146.wildan@mail.unpas.ac.id>
2021-10-14 21:17:30 +0700

    Menyelesaikan Video 5, Mengenai Model, Collection & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
+// use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,65 +32,5 @@ Route::get('/About', function () {
     ]);
 });
 
-
-
-Route::get('/Blog', function () {
-
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Wildan Prasetyo",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio ipsum reprehenderit nam porro voluptas non hic aliquam voluptatem? Deserunt dignissimos nihil enim aut soluta at adipisci commodi quae illum inventore repellendus earum modi, minima rem atque laborum quo doloremque consequuntur eius numquam suscipit explicabo natus. Soluta asperiores consectetur tempora, pariatur expedita vitae. Corrupti ducimus illum architecto hic nemo blanditiis odit excepturi est corporis sequi inventore doloremque itaque minima exercitationem porro repellendus explicabo consequuntur quae dolores similique quis, fugiat distinctio aliquid?"
-        ],
-    
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Asep Galon",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio ipsum reprehenderit nam porro voluptas non hic aliquam voluptatem? Deserunt dignissimos nihil enim aut soluta at adipisci commodi quae illum inventore repellendus earum modi, minima rem atque laborum quo doloremque consequuntur eius numquam suscipit explicabo natus. Soluta asperiores consectetur tempora, pariatur expedita vitae. Corrupti ducimus illum architecto hic nemo blanditiis odit excepturi est corporis sequi inventore doloremque itaque minima exercitationem porro repellendus explicabo consequuntur quae dolores similique quis, fugiat distinctio aliquid?"
-        ]
-    ];
-
-    return view('Posts', [
-    "title" => "Posts",
-    "posts" => $blog_posts
-    ]);
-});
-
-
-
-// halaman singel post
-
-Route::get('posts/{slug}', function ($slug) {
-
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Wildan Prasetyo",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio ipsum reprehenderit nam porro voluptas non hic aliquam voluptatem? Deserunt dignissimos nihil enim aut soluta at adipisci commodi quae illum inventore repellendus earum modi, minima rem atque laborum quo doloremque consequuntur eius numquam suscipit explicabo natus. Soluta asperiores consectetur tempora, pariatur expedita vitae. Corrupti ducimus illum architecto hic nemo blanditiis odit excepturi est corporis sequi inventore doloremque itaque minima exercitationem porro repellendus explicabo consequuntur quae dolores similique quis, fugiat distinctio aliquid?
-            "
-        ],
-    
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Asep Galon",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio ipsum reprehenderit nam porro voluptas non hic aliquam voluptatem? Deserunt dignissimos nihil enim aut soluta at adipisci commodi quae illum inventore repellendus earum modi, minima rem atque laborum quo doloremque consequuntur eius numquam suscipit explicabo natus. Soluta asperiores consectetur tempora, pariatur expedita vitae. Corrupti ducimus illum architecto hic nemo blanditiis odit excepturi est corporis sequi inventore doloremque itaque minima exercitationem porro repellendus explicabo consequuntur quae dolores similique quis, fugiat distinctio aliquid?
-            "
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] == $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
